d8d5bd294c8dd49f4dd1dd98a4660cc8d17b173f
Haui <jacobgh00@gmail.com>
2023-02-10 10:31:04 +0100

    opg5 - 1

--- versionstyring/Opgave5.java
+++ versionstyring/Opgave5.java
@@ -5,9 +5,30 @@ import java.util.List;
 public class Opgave5 {
 
 
+//Lav metoden under anvendelse af iteration
 
-    public static int countDelStreng(List<String> list, String sub){
-
-        return 0;
+    public static int countDelStreng1(List<String> list, String sub) {
+        int count = 0;
+        for (String s : list) {
+            if (s.contains(sub)) {
+                count++;
+            }
+        }
+        return count;
     }
+
+
+
+//Lav metoden så der kigges på et element og der derefter kaldes rekursivt med resten af listen
+
+
+
+
+
+    //Third option
+
+
+
+
 }
+
