d5e16e0c18de731e894e0af5faf4431331350d29
Haui <jacobgh00@gmail.com>
2023-02-10 09:52:25 +0100

    opg5

---
+++ versionstyring/Opgave5.java
@@ -0,0 +1,13 @@
+package versionstyring;
+
+import java.util.List;
+
+public class Opgave5 {
+
+
+
+    public static int countDelStreng(List<String> list, String sub){
+
+        return 0;
+    }
+}
