1ab495e5d81d26719246445af4272cf6c7bf4fba
louisemogensen <louisemogensen2@gmail.com>
2023-02-10 11:27:21 +0100

    konflikt test

--- Silas.java
+++ Silas.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Silas {
     // Louise er en taber
     public static void main(String[] args) {
@@ -5,5 +8,25 @@ public class Silas {
         System.out.println("Inaktive flødebolle dreng");
         System.out.println("Udstoppet neger");
 
+    }
+
+    public int countDelStreng2(List<String> list, String sub) {
+        List<String> test = new ArrayList<>();
+        test.add("Jens Jensen");
+        test.add("Jens Hansen");
+        test.add("Jan Jensen");
+        test.add("Leo Hansen");
+        test.add("Jensine Larsen");
+
+        int result = 0;
+        int index = 0;
+
+        if(list.isEmpty()) {
+            return result;
+
+        } else {
+
+        }
+        return result;
     }
 }
