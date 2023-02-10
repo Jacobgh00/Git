7e58b3d34d19db0e176c7cb0e945171739a71862
silasskovchristensen <silasskovchristensen@gmail.com>
2023-02-10 11:54:28 +0100

    sandhed

--- Silas.java
+++ Silas.java
@@ -4,13 +4,7 @@ import java.util.List;
 public class Silas {
     // Louise er en taber
     public static void main(String[] args) {
-        System.out.println("Silas er overvægtig");
-        System.out.println("Inaktive flødebolle dreng");
-        System.out.println("Udstoppet neger");
 
-    }
-
-    public int countDelStreng2(List<String> list, String sub) {
         List<String> test = new ArrayList<>();
         test.add("Jens Jensen");
         test.add("Jens Hansen");
@@ -18,15 +12,32 @@ public class Silas {
         test.add("Leo Hansen");
         test.add("Jensine Larsen");
 
-        int result = 0;
-        int index = 0;
+        Silas sild = new Silas();
+        System.out.println(sild.countDelStreng2(test, "Jens", 0, 0));
+    }
 
-        if(list.isEmpty()) {
-            return result;
+    public int countDelStreng2(List<String> list, String sub, int index, int count) {
 
+        if(index <= list.size()) {
+            return count;
         } else {
+            countDelStreng2(list, sub, index++, count + hjælp(list.get(index),sub));
+        }
+        return count;
+    }
 
+    public int hjælp(String s, String sub) {
+        int result = 0;
+        int startIndex = 0;
+        int slutIndex = startIndex + sub.length();
+        while (slutIndex < s.length()) {
+            if (s.substring(startIndex, slutIndex).contains(sub)) {
+                result++;
+            }
+            startIndex++;
+            slutIndex++;
         }
         return result;
     }
+
 }
