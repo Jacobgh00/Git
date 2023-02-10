e0bf2b9915dc28d05520b8b43587f17d5077d198
Haui <jacobgh00@gmail.com>
2023-02-10 10:44:19 +0100

    opg5 - 1

--- Silas.java
+++ Silas.java
@@ -1,3 +1,9 @@
 public class Silas {
     // Louise er en taber
+    public static void main(String[] args) {
+        System.out.println("Silas er overvægtig");
+        System.out.println("Inaktive flødebolle dreng");
+        System.out.println("Udstoppet neger");
+
+    }
 }
